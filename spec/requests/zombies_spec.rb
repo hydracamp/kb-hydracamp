229b46bc08fc4b9d5d0266650ce85f3bcdbae7ad
Eld <elzi@kb.dk>
2012-10-22 19:49:36 +0200

    Added test for the tweets

--- spec/requests/zombies_spec.rb
+++ spec/requests/zombies_spec.rb
@@ -10,5 +10,21 @@ describe "Zombies" do
       page.should have_content "Added Zombie"
       page.should have_content "Ash"
   end
+
+  before do
+    @zombie = Zombie.create(:name=>"Bones", :graveyard=>"SB")
+  end
+
+  it "should create tweets" do
+    #Given I am viewing the list of tweets for a zombie
+    visit zombie_path @zombie
+    #When I fill in the message of "This is my tweet"
+    fill_in "Message", :with => 'This is my Tweet'
+    #And I click "Create Tweet"
+    click_button "Create Tweet"
+    #Then I should return to the list of tweets for the zombie
+    #And I should see "This is my tweet" at the top of the list of tweets
+    page.should have_selector "ul li:first", :text=>'This is my Tweet'
+  end
 end
 
