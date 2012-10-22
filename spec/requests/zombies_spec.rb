40cbc0904cbb74b158cba58c2e127839a71d6b70
Blimey74 <jatr@kb.dk>
2012-10-22 19:51:40 +0200

    Adding in missing directory and files

---
+++ spec/requests/zombies_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Zombies" do
+  it "should create a zombie" do
+      visit new_zombie_path
+      fill_in "Name", :with =>'Ash'
+      fill_in "Graveyard", :with =>'Milltown'
+
+      click_button "Create"
+      page.should have_content "Added Zombie"
+      page.should have_content "Ash"
+  end
+end
+
