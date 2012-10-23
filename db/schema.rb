b71334ed46d3257275207e1aa14ae273391c547d
Kristoffer Brinch Kjeldby <kristoffer@kjeldby.dk>
2012-10-23 16:26:22 +0200

    readded zombies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022163818) do
+ActiveRecord::Schema.define(:version => 20121023131012) do
 
   create_table "tweets", :force => true do |t|
     t.string   "message"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(:version => 20121022163818) do
   end
 
   add_index "tweets", ["zombie_id"], :name => "index_tweets_on_zombie_id"
+
+  create_table "weapons", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "zombies", :force => true do |t|
     t.string   "name"
